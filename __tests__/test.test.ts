989a3646c9a063ffb2f0fab901dfb28d4f24aac2
Kristjan Kosic - Chris <chris@ark.io>
2019-10-21 10:42:11 +0200

    test: updated to match new structure

--- __tests__/test.test.ts
+++ __tests__/test.test.ts
@@ -12,10 +12,12 @@ describe("Test builder",()=>{
 
         const builder = new BusinessRegistrationBuilder();
         const actual = builder
-            .businessAsset("google","www.google.com")
+            .businessData("google","www.google.com")
             .nonce("3")
             .sign("clay harbor enemy utility margin pretty hub comic piece aerobic umbrella acquire");
 
+
+        console.log(actual.build().toJson());
         expect(actual.build().verified).toBeTrue();
         expect(actual.verify()).toBeTrue();
     });
